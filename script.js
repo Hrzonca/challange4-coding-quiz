4e3377286e163c95d1846925bb282d60136e8860
Haleigh Rzonca <hgielah@gmail.com>
2022-09-07 13:46:39 -0700

    Trying to get the questions to show up once the start button is clicked

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-var quizRunning = true;
-var questionAnswer = 0;
+var quizRunning = true; //if there is still time then the quiz is true
+var questionAnswered = 0; //track the number of questions answered
+var trackAnswer = 0;
+var score = 0; //baseline score
+var highScore = 50; //Max score
 var score = document.getElementById('topscore');
 var start = document.getElementsByClassName("start");
-var question1El = document.getElementsById('question1');
-var question2El = document.getElementsById('question2');
-var question3El = document.getElementsById('question3');
-var question4El = document.getElementsById('question4');
-var question5El = document.getElementsById('question5');
+var question1El = document.getElementById('question1');
+var question2El = document.getElementById('question2');
+var question3El = document.getElementById('question3');
+var question4El = document.getElementById('question4');
+var question5El = document.getElementById('question5');
 var submitScoreEl = document.getElementById('sumbitscore');
+var displayQuestion = document.createElement('questionDisplay')
 
 question1El.style.display = 'none';
 question2El.style.display = 'none';
@@ -16,6 +20,9 @@ question4El.style.display = 'none';
 question5El.style.display = 'none';
 submitScoreEl.style.display = 'none';
 
+document.getElementById('start').onclick = function () {
+    anatomyQuestion
+}
 var anatomyQuestion = {
     questions: {
         0: 'How many bones are in the human body?',
@@ -32,36 +39,80 @@ var anatomyAnswers = {
             0: "404",
             1: '3',
             2: '206',
-            3: '372'
+            3: '372',
         },
         1: {
             0: 'Monocyte',
             1: 'Basophil',
             2: 'Erythroctye',
-            3: 'Lymphocyte'
+            3: 'Lymphocyte',
         },
         2: {
             0: 'Sebum',
             1: 'Melanin',
             2: 'Elastin',
-            3: 'Collagen'
+            3: 'Collagen',
         },
         3: {
-            0: 'Optic',
+            0: 'Olfactory',
             1: 'Trigeminal',
-            2: 'Olfactory',
-            3: 'Facial'
+            2: 'Optic',
+            3: 'Facial',
         },
         4: {
-            0: 'Carbon Dioxide',
-            1: 'Carbon monoxide',
+            0: 'Carbon monoxide',
+            1: 'Carbon dixide',
             2: 'Xenon',
-            3: 'Nitrogen'
+            3: 'Nitrogen',
         },
     }
 
 }
 
 var correctAnswers = {
+    0: '2',
+    1: '2',
+    2: '3',
+    3: '0',
+    4: '1',
 
+}
+
+//whichever answer the user chooses, they are prompted if it is right or wrong. if right then the quiz moves on and add 1 second. if wrong the quiz moves on but 5 seconds is removed. has to be done with all the questions. 
+question1El.addEventListener('click', function () {
+if (questionDisplay.textContent === 'Which of the following substances is found in greater quantity in exhaled air?' && question5El === "Carbon monoxide") {
+    console.log("Correct!")
+    questionAnswered = 2;
+    trackAnswer = 
+}
+    
+})
+
+function question2El() {
+    if (userChoice === '2') {
+        alert('Correct!');
+    } else {
+        alert('Wrong!')
+    }
+}
+function question3El() {
+    if (userChoice === '3') {
+        alert('Correct!');
+    } else {
+        alert('Wrong!')
+    }
+}
+function question4El() {
+    if (userChoice === '0') {
+        alert('Correct!');
+    } else {
+        alert('Wrong!')
+    }
+}
+function question5El() {
+    if (userChoice === '1') {
+        alert('Correct!');
+    } else {
+        alert('Wrong!')
+    }
 }
